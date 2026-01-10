54fb36e8ed6bba4a0413f8d07e9af8609d2d2978
Andrii <andriykostiuk1@gmail.com>
2026-01-10 17:20:40 +0200

    add layout structure

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+import { Header } from '@/common/components/patterns/header/header'
+
+export default function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={'main-layout'}>
+      <Header />
+      {/*<Sidebar />*/}
+
+      <main className={'main-layout__content'}>{children}</main>
+    </div>
+  )
+}
---
+++ src/common/components/patterns/header/header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header className={'header'}>
+      <div className="header__container">
+        <div className="header__left"></div>
+
+        <div className="header__center"></div>
+
+        <div className="header__right"></div>
+      </div>
+    </header>
+  )
+}
